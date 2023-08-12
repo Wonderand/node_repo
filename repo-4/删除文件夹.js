0147ea6196527554853bab75e4719ba4a9e1f0d4
hzr <2290641663@qq.com>
2023-08-12 09:30:23 +0800

    创建读取文件夹

--- repo-4/删除文件夹.js
+++ repo-4/删除文件夹.js
@@ -1,15 +1,15 @@
 /**
- * ClassName: 文件删除
+ * ClassName: 删除文件夹
  * Description:
  * @Author: hzr
- * Date: 2023/8/12 9:11
+ * Date: 2023/8/12 9:20
  * History:
  * <author>          <time>          <version>          <desc>
- * hzr    2023/8/12 9:11   @Version 1.0        描述
+ * hzr    2023/8/12 9:20   @Version 1.0        描述
  */
 const fs = require('fs');
-//2.删除文件
-// fs.rmdir('../repo-1/重命名观书有感.txt',err=>{
+//2.删除文件夹
+// fs.rmdir('./创建文件夹',err=>{
 //     if(err){
 //         console.log(err);
 //         return;
@@ -17,10 +17,10 @@ const fs = require('fs');
 //     console.log('删除成功');
 // });
 //3.递归删除文件夹
-fs.rm('./创建文件夹/',{recursive: true},err=>{
+fs.rm('./a',{recursive: true},err=>{
     if(err){
         console.log(err);
         return;
     }
     console.log('删除成功');
-});
+})
